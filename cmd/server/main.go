ccc23fce729c73b7838e8d9c337e6f7f088dee09
KandakatlaChandramouli <2303031240495@paruluniversity.ac.in>
2026-05-21 09:10:38 +0000

    checkpoint

---
+++ cmd/server/main.go
@@ -0,0 +1,68 @@
+
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"fluxruntime/internal/core"
+	"fluxruntime/internal/transport"
+)
+
+func main() {
+
+	ctx, cancel := context.WithCancel(
+		context.Background(),
+	)
+
+	defer cancel()
+
+	modelPath := "models/model.onnx"
+
+	pool, err := core.NewShardedPool(
+		ctx,
+		modelPath,
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer pool.Shutdown()
+
+	server := transport.NewServer(
+		":7001",
+		pool,
+	)
+
+	go func() {
+
+		sig := make(
+			chan os.Signal,
+			1,
+		)
+
+		signal.Notify(
+			sig,
+			syscall.SIGINT,
+			syscall.SIGTERM,
+		)
+
+		<-sig
+
+		cancel()
+	}()
+
+	log.Println(
+		"runtime listening on :7001",
+	)
+
+	err = server.Listen(ctx)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
