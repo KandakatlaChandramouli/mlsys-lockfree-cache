3c4b23dcf1e5a1ed0470944a4aba26f8e7b08037
KandakatlaChandramouli <2303031240495@paruluniversity.ac.in>
2026-05-21 05:56:15 +0000

    checkpoint

--- internal/lockfree/ring.go
+++ internal/lockfree/ring.go
@@ -1,54 +1,38 @@
 package lockfree
 
-import "sync/atomic"
-
-const RingSize = 4096
-
 type Ring struct {
-	head atomic.Uint64
-	tail atomic.Uint64
-
-	buf [RingSize]any
+	ch chan any
 }
 
 func NewRing() *Ring {
 
-	return &Ring{}
+	return &Ring{
+		ch: make(chan any, 4096),
+	}
 }
 
 func (r *Ring) Push(
 	v any,
 ) bool {
 
-	head := r.head.Load()
-	tail := r.tail.Load()
+	select {
 
-	if head-tail >= RingSize {
+	case r.ch <- v:
+		return true
+
+	default:
 		return false
 	}
-
-	idx := head % RingSize
-
-	r.buf[idx] = v
-
-	r.head.Add(1)
-
-	return true
 }
 
 func (r *Ring) Pop() (any, bool) {
 
-	tail := r.tail.Load()
+	select {
 
-	if tail >= r.head.Load() {
+	case v := <-r.ch:
+		return v, true
+
+	default:
 		return nil, false
 	}
-
-	idx := tail % RingSize
-
-	v := r.buf[idx]
-
-	r.tail.Add(1)
-
-	return v, true
 }
